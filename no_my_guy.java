44cbc1dbd0bb2fcc93005a347534c5747d8a3392
meep-2810 <vanasluc000@stu.asd103.org>
2025-01-14 01:20:56 +0000

    good

---
+++ no_my_guy.java
@@ -0,0 +1,19 @@
+public class no_my_guy
+{
+    public static void main(String[] args)
+    {
+        String nameno = "Lucas Portugal Vanasse";
+        int n = 100;
+        System.out.println(n);
+        // n = n + 10;
+        // sjhdfjfs fhsldf
+
+        n += 10;
+        System.out.println(n);
+        String output = "";
+
+        output = "no " + nameno;
+        System.out.println( output);
+        
+    }
+}
